a9ea41035d4c1c4492b2b982d25bd5b7dcc2d67b
Andrey Glotov <Andruxa.comm@gmail.com>
2018-06-27 16:44:37 +0300

    HOC for server work

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+import withCrud from './components/hoc/withCrud';
+
+function App({data, create, update, remove}) {
+    return (
+        <div className='container'>
+            <Form onSubmit ={create}/>
+            <List   todos={data}
+                    onToggle={update}
+                    onRemove={remove}
+            />
+        </div>
+    )
+}
+
+export default withCrud(App, 'api/todos');
